b150c9ad415cab0612ba4026adf4e4aa433122a7
jpope6 <imthepope@csu.fullerton.edu>
2023-01-10 11:02:36 -0800

    clicking makes x/o draw

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 // module for gameboard class
 const gameboard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
+  let turn = "X";
 
   const container = document.getElementById("board");
 
   const createBoard = () => {
     for (let i = 0; i < board.length; i++) {
       let space = document.createElement("div");
+      space.addEventListener("click", () => takeTurn(space));
       container.appendChild(space);
+    }
+  };
+
+  // draw X or O in space and change turn to next player
+  const takeTurn = (space) => {
+    if (space.textContent) {
+      return;
+    }
+
+    space.textContent = turn;
+    if (turn == "X") {
+      turn = "O";
+    } else {
+      turn = "X";
     }
   };
 
